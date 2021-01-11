b7fa7233f3eb9f929d9852a5b413390690e53657
Justin <justin82049@gmail.com>
2021-01-11 13:21:32 -0500

    finished nth

--- Radix.java
+++ Radix.java
@@ -1,6 +1,9 @@
 public class Radix{
   public static int nth(int n, int col){
     int amountTodivide = Math.pow(10,col);
+    n /= amountTodivide;
+    n = n % amountTodivide;
+    return n;
   }
   
   
