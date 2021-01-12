7a5d89136a5e3a8fd003a71792d9a071618aa9e9
Justin <justin82049@gmail.com>
2021-01-12 18:21:36 -0500

    typedcast to int and rounded because the original outputs were doubles, added a tester in Radix

--- Radix.java
+++ Radix.java
@@ -1,10 +1,20 @@
+import java.lang.*;
+
 public class Radix{
   public static int nth(int n, int col){
-    int amountTodivide = Math.pow(10,col);
+    int amountTodivide = (int) Math.round(Math.pow(10,col));
     n /= amountTodivide;
     n = n % amountTodivide;
     return n;
   }
+  public static int length(int n){
+    int tens = (int) Math.round(Math.log10(n));
+    return tens + 1;
+  }
   
-  
+  public static void main (String[]agrs){
+    System.out.println(nth(123,1));
+    System.out.println(length(102));
+  }
+
 }
