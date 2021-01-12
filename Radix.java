cfe2c112b5a8cc02b1f8bbae226eb54166008515
Justin <justin82049@gmail.com>
2021-01-12 18:25:07 -0500

    made it so length works for negatives and used .floor to always round down

--- Radix.java
+++ Radix.java
@@ -2,19 +2,22 @@ import java.lang.*;
 
 public class Radix{
   public static int nth(int n, int col){
-    int amountTodivide = (int) Math.round(Math.pow(10,col));
+    int amountTodivide = (int) Math.floor(Math.pow(10,col));
     n /= amountTodivide;
     n = n % amountTodivide;
     return n;
   }
   public static int length(int n){
-    int tens = (int) Math.round(Math.log10(n));
+    if (n < 0){
+      n *= -1;
+    }
+    int tens = (int) Math.floor(Math.log10(n));
     return tens + 1;
   }
   
   public static void main (String[]agrs){
     System.out.println(nth(123,1));
-    System.out.println(length(102));
+    System.out.println(length(-10));
   }
 
 }
