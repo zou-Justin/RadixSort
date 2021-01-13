014e517106de13bb0e091d2e152a41cfc0c058f2
Justin <justin82049@gmail.com>
2021-01-12 19:04:55 -0500

    tested and made sure nth did not output negatives

--- Radix.java
+++ Radix.java
@@ -5,7 +5,7 @@ public class Radix{
     int amountTodivide = (int) Math.floor(Math.pow(10,col));
     n /= amountTodivide;
     n = n % amountTodivide;
-    return n;
+    return Math.abs(n);
   }
   public static int length(int n){
     if (n < 0){
@@ -14,8 +14,14 @@ public class Radix{
     int tens = (int) Math.floor(Math.log10(n));
     return tens + 1;
   }
-  
+  public static void merge(MyLinkedList original,MyLinkedList[]buckets){
+    
+  }
   public static void main (String[]agrs){
+    System.out.println(nth(123,1));
+    System.out.println(nth(-123,1));
+    System.out.println(nth(123,2)); 
+    System.out.println(nth(-123,2));
     System.out.println(nth(123,1));
     System.out.println(length(-10));
   }
